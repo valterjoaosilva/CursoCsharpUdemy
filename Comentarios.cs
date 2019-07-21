6d06dcd8114c4341a5b9a06debc090e64e46c2dd
valter João da Silva Junior <valterjoaosilva@gmail.com>
2019-07-21 13:54:21 -0300

    arquivos do C#

---
+++ Comentarios.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class Comentarios
+    {
+        public static void Executar()
+        {
+           Console.WriteLine("código  claro é sempre melhor!");
+            //aula de comentário temos diversos modelos esse é o comentário de linha damos duas barras
+
+           /*
+            * Esse é o comentário de bloco, e funciona para varias linhas
+            * os asteriscos na frente da linha não são necessário apenas depois e antes da barra inicial e final
+            */
+             
+           ///<
+           ///comentario de sumario, da opção do comando que você quer comentar.
+      
+
+            
+             
+        }
+    }
+}
