19a3536fbf0f428b7d2efa3ad90f41605bd17851
valter João da Silva Junior <valterjoaosilva@gmail.com>
2019-07-21 22:47:36 -0300

    aula inferencia

---
+++ Fundamentos/Inferencia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class Inferencia
+    {
+        public static void Executar()
+        {
+
+        }
+    }
+}
