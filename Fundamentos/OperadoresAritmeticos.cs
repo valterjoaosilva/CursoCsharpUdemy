16524946a941ab855477748bf4dd72a4b4df5711
valter João da Silva Junior <valterjoaosilva@gmail.com>
2019-07-25 16:57:58 -0300

    final dos commits para fundamentos

---
+++ Fundamentos/OperadoresAritmeticos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class OperadoresAritmeticos
+    {
+        public static void Executar()
+        {
+            // operações padrões 
+
+            var preco = 234.56;
+            var imposto = 1.05;
+            var lucro = 1.10;
+            var desconto = 0.2;
+            var total = ((preco * lucro) * (imposto)) - (((preco * lucro) * (imposto)) * desconto);
+            Console.WriteLine("Valor total é: {0}", total);
+
+            // calculo
+
+            var raio = 3.5;
+            var lado = 4.2;
+            const double pi = 3.14;
+            var area = Math.Pow(raio, 2) * pi;
+            var quadrado = Math.Pow(lado, 2);
+            Console.WriteLine($"A área do Raio é: {area} área do quadrado é: {quadrado}");
+
+            int valor1 = 4;
+            int valor2 = 7;
+            int valorPar = valor1 % 2;
+            int valorImpar = valor2 % 2;
+            Console.WriteLine("valor par: {0} valor impar: {1}", valorPar, valorImpar);
+
+
+
+
+        }
+    }
+}
---
+++ Fundamentos/OperadoresRelacionais.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class OperadoresRelacionais
+    {
+        public static void Executar()
+        {
+            double nota = 6.0;
+            double notaprovacao = 7.0;
+
+            Console.WriteLine("nota inválida? {0}", nota > 10.0);
+            Console.WriteLine("Aprovado com louvor? {0}", nota == 10.0);
+            Console.WriteLine("Aprovado? {0}", nota >= 7.0);
+            Console.WriteLine("Ficou em Recuperação? {0}", nota < 7.0);
+            Console.WriteLine("Reprovado? {0}", nota < 3.0);
+
+            Console.WriteLine("Qual é a nota do aluno: ");
+            double.TryParse(Console.ReadLine(), out double nota1 );
+
+            Console.WriteLine("nota inválida? {0}", nota1 > 10.0);
+            Console.WriteLine("Aprovado com louvor? {0}", nota1 == 10.0);
+            Console.WriteLine("Aprovado? {0}", nota1 >= 7.0);
+            Console.WriteLine("Ficou em Recuperação? {0}", nota1 < 7.0);
+            Console.WriteLine("Reprovado? {0}", nota1 < 3.0);
+
+
+
+
+
+        }
+    }
+}
