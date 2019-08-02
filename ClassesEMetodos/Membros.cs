e7bc93c92dd20be02cc62dfbfee12a09acabe986
valter João da Silva Junior <valterjoaosilva@gmail.com>
2019-08-02 15:52:52 -0300

    nova estrutura

---
+++ ClassesEMetodos/Membros.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.CursoCsharpUdemy.ClassesEMetodos
+{
+    class Membros
+    {
+    }
+}
---
+++ ClassesEMetodos/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    class Pessoa
+    {
+        public string Nome; //atributo publico
+        public int Idade; //atributo publico
+
+    }
+}
