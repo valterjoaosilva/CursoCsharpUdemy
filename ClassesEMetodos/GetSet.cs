fb28c3e1e46c09d60b2585a6d9fdcf255602ac2e
valter João da Silva Junior <valterjoaosilva@gmail.com>
2019-08-22 15:34:47 -0300

    incluindo mais arquivos do curso

--- ClassesEMetodos/GetSet.cs
+++ ClassesEMetodos/GetSet.cs
@@ -4,13 +4,65 @@ using System.Text;
 
 namespace CursoCSharp.ClassesEMetodos
 {
+    public class Moto
+    {
+        private string Marca;
+        private string Modelo;
+        private uint Cilindrada;
 
+        public Moto(string marca, string modelo, uint cilindrada)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Cilindrada = cilindrada;
+        }
+        public Moto()
+        {
+
+        }
+
+        public string GetMarca()
+        {
+            return Marca;
+
+        }
+        public void SetMarca(string marca)
+        {
+            Marca = marca;
+        }
+        public string GetModelo()
+        {
+            return Modelo;
+        }
+        public void SetModelo(string modelo)
+        {
+            Modelo = modelo;
+        }
+        public uint GetCilindrada()
+        {
+            return Cilindrada;
+        }
+        public void SetCilindrada(uint cilindrada)
+        {
+            Cilindrada = cilindrada;
+        }
+    }
 
     class GetSet
     {
         public static void Executar()
         {
+            var moto1 = new Moto("Kavasaki", "Ninja ZX-6R", 635);
 
+            Console.WriteLine(moto1.GetMarca());
+            Console.WriteLine(moto1.GetModelo());
+            Console.WriteLine(moto1.GetCilindrada());
+
+            var moto2 = new Moto();
+            moto2.SetMarca("Honda");
+            moto2.SetModelo("CG Titan");
+            moto2.SetCilindrada(150);
+            Console.WriteLine(moto2.GetMarca() + " " + moto2.GetModelo() + " " + moto2.GetCilindrada());
         }
     }
 }
---
+++ ClassesEMetodos/Props.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    public class CarroOpcional
+    {
+        double desconto = 0.1;
+
+        string nome; 
+        public string Nome
+        {
+            get
+            {
+                return "Opcional: " + nome;
+            }
+            set
+            {
+                nome = value;
+            }
+            
+        }
+        // Propriedade autoimplementada
+        public double Preco { get; set; }
+        // Sometn leitura
+        public double PrecoComDesconto
+        {
+            get => Preco - (desconto * Preco); // Lambda
+            //get {
+            //return Preco - (descibti * Preco);
+            //}
+        }
+        public CarroOpcional()
+        {
+
+        }
+
+        public CarroOpcional(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+
+        }
+    }
+    class Props
+    {
+        public static void Executar()
+        {
+            var op1 = new  CarroOpcional("Ar Condicionado ", 3499.9);
+            Console.WriteLine(op1.PrecoComDesconto);
+
+            var op2 = new CarroOpcional();
+            op2.Nome = "Direção Elétrica";
+            op2.Preco = 2349.9;
+
+            Console.WriteLine(op1.Nome);
+            Console.WriteLine(op1.Preco);
+
+            Console.WriteLine(op2.Nome);
+            Console.WriteLine(op2.Preco);
+            Console.WriteLine(op2.PrecoComDesconto);
+        }
+    }
+}
